3bb4ba29c349cd5001105602718fac9bfeee44e2
hao <xiaohao@haosblog.com>
2017-08-14 18:20:01 +0800

    create Application

---
+++ src/Application.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *  entrance of the wiki sdk
+ *
+ * @create 2017-8-14 18:10:29
+ * @author hao
+ */
+
+class Application
+{
+    
+    /**
+     * the domain of the wiki website
+     * 
+     * @var string
+     */
+    protected $wikiDomain = '';
+
+    /**
+     * the query action instance
+     * 
+     * @var type 
+     */
+    protected $actionQuery;
+    
+    /**
+     * construct
+     * 
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $this->wikiDomain = $config['domain'];
+    }
+    
+    
+    public function getQuery(){
+        if(!$this->actionQuery instanceof Query){
+            
+        }
+    }
+}
