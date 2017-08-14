a77950f58c7e54199117d6b00969ba12ba41b0e4
hao <xiaohao@haosblog.com>
2017-08-14 18:23:30 +0800

    create AbstractQuery

---
+++ src/Query/AbstractQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of AbstractQuery
+ *
+ * @create 2017-8-14 18:21:53
+ * @author hao
+ */
+
+namespace Haosblog\WikiSDK\Query;
+
+use Haosblog\WikiSDK\Core\AbstractAPI;
+
+
+class AbstractQuery extends AbstractAPI
+{
+    //put your code here
+}
